44ad14b506b9d6736d9b87ded54892fd12934008
Kaustav Paul <kaustavpaul347@gmail.com>
2026-03-15 19:24:51 +0530

    Code cleanup for fresh start

--- src/main/java/com/territory/territory/repository/ExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.territory.territory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.territory.territory.entity.ExampleEntity;
-
-public interface ExampleRepository
-        extends JpaRepository<ExampleEntity, Long> {
-}

8ab515914a0e2930614e850ffd3032e197a04444
Kaustav Paul <kaustavpaul347@gmail.com>
2026-03-20 00:59:39 +0530

    Build fix

---
+++ src/main/java/com/territory/territory/TerritoryApplication.java
@@ -0,0 +1,12 @@
+package com.territory.territory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerritoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerritoryApplication.class, args);
+    }
+}
